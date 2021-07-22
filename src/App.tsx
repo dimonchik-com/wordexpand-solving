3f2875e71857fec77ed266cc4d545bdbaed61302
Dmytro Kupriianov <dmitriy.dn.ua@gmail.com>
2021-07-23 01:32:36 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { shuffle } from "lodash";
+import "./App.css";
+// @ts-ignore
+import * as words from "categorized-words";
 
 function App() {
+  const [type, setType] = useState("N");
+  const [seachValue, updateSearch] = useState("");
+  const [strLen, setStrLen] = useState("");
+  const inputRef = useRef<any>(null);
+
+  const clickButton = (key: string) => {
+    setType(key);
+    inputRef.current.focus();
+  };
+
+  const startTyping = (event: any) => {
+    updateSearch(event.target.value);
+  };
+
+  const result = shuffle(
+    words[type].filter(
+      (element: string) =>
+        element.startsWith(seachValue) &&
+        (!strLen || element.length === +strLen)
+    )
+  )
+    .sort((a: string, b: string) => a.length - b.length)
+    .slice(0, 200);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className={type === "N" ? "active" : "p1"}
+        onClick={() => clickButton("N")}
+      >
+        nouns
+      </button>
+      <button
+        className={type === "V" ? "active" : "p1"}
+        onClick={() => clickButton("V")}
+      >
+        verbs
+      </button>
+      <button
+        className={type === "A" ? "active" : "p1"}
+        onClick={() => clickButton("A")}
+      >
+        adjectives
+      </button>
+      <input
+        size={3}
+        value={strLen}
+        onChange={(e) => setStrLen(e.target.value)}
+      />
+      <hr />
+      <input
+        type="text"
+        value={seachValue}
+        className="p2"
+        onChange={startTyping}
+        ref={inputRef}
+      />
+      <input type="submit" value="Clear" onClick={() => updateSearch("")} />
+      {result.map((element: string) => {
+        return <p>{element}</p>;
+      })}
+      <hr />
     </div>
   );
 }
